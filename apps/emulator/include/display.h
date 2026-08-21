5f2b7826f8f84b440d3c54aa0e2706f98b9b387b
PurrRock <purrrock@gmail.com>
2026-08-21 09:38:29 +0300

    display.h

--- apps/emulator/include/display.h
+++ apps/emulator/include/display.h
@@ -2,21 +2,41 @@
 #define EMULATOR_DISPLAY_H
 
 #include <stdint.h>
+#include "purrgo/hardware_config.h"
 
-#define DISPLAY_WIDTH  128
-#define DISPLAY_HEIGHT 296
-#define DISPLAY_BPP    2 // 2 bits per pixel
-#define DISPLAY_FB_SIZE ((DISPLAY_WIDTH * DISPLAY_HEIGHT * DISPLAY_BPP) / 8)
+/*
+ * Геометрия дисплея и глубина цвета транслируются напрямую
+ * из активного профиля аппаратной конфигурации.
+ */
+#define DISPLAY_WIDTH       PURRGO_HW_DISPLAY_WIDTH_PX
+#define DISPLAY_HEIGHT      PURRGO_HW_DISPLAY_HEIGHT_PX
+#define DISPLAY_BPP         PURRGO_HW_DISPLAY_BPP
 
-// Color values (2-bit)
-#define COLOR_BLACK     0x00
-#define COLOR_DARK_GRAY 0x01
-#define COLOR_LIGHT_GRAY 0x02
-#define COLOR_WHITE     0x03
+/*
+ * Размер кадрового буфера в байтах:
+ * (Ширина * Высота * Бит_на_пиксель) / 8 бит в байте
+ */
+#define DISPLAY_FB_SIZE     ((DISPLAY_WIDTH * DISPLAY_HEIGHT * DISPLAY_BPP) / 8)
+
+/* 
+ * 2-битная палитра градаций серого (логические уровни):
+ * 00b = Black, 01b = Dark Gray, 10b = Light Gray, 11b = White
+ */
+#define COLOR_BLACK         0x00
+#define COLOR_DARK_GRAY     0x01
+#define COLOR_LIGHT_GRAY    0x02
+#define COLOR_WHITE         0x03
 
 void display_init(void);
 void display_clear(uint8_t color);
-void display_set_pixel(int x, int y, uint8_t color);
+
+/*
+ * Установка пикселя в кадровый буфер.
+ * Используются явные типы int16_t для координат с целью предотвращения
+ * неявных приведений типов и корректной работы знакового отсечения (clipping).
+ */
+void display_set_pixel(int16_t x, int16_t y, uint8_t color);
+
 const uint8_t* display_get_framebuffer(void);
 
 #endif // EMULATOR_DISPLAY_H
