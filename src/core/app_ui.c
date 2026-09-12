7f967f448425031f2f423a0d6bf8d782a9f4ef66
Litvin Vasil <purrrock@gmail.com>
2026-09-12 10:07:52 +0300

    Move ui_draw_key_hints

--- src/core/app_ui.c
+++ src/core/app_ui.c
@@ -77,6 +77,7 @@ void purrgo_app_ui_render(
             ui_trip_render_values(gfx, gnss, sun);
             break;
         case APP_STATE_MAP:
+            ui_draw_key_hints(gfx, current_state);
             ui_render_map(gfx, gnss, sun);
             break;
         case APP_STATE_MENU_DIR_SELECT:
@@ -89,7 +90,5 @@ void purrgo_app_ui_render(
             break;
     }
 
-    ui_draw_key_hints(gfx, current_state);
-
-    prev_state = current_state;
+      prev_state = current_state;
 }
